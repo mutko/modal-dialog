81f7fe0ecc1cea4fe918316175e5156f8d695f48
Branko Bozovic <branko028@yahoo.com>
2019-03-03 09:10:15 +0100

    Polishing touches

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ const closeModal = q('.close-modal');
 const overlay = q('.overlay');
 const modal = q('.modal');
 
-modalOpen.addEventListener('click', function() {
-  overlay.classList.add('active');
-  modal.classList.add('active');
+modalOpen.addEventListener('click', function() { 
+    overlay.classList.add('active');
+    modal.classList.add('active'); 
+    document.body.style.overflow = "hidden";
 })
 
 closeModal.addEventListener('click', function() {
   overlay.classList.remove('active');
-  modal.classList.remove('active');
 })
 
 // close modal on click outside modal
